4764b3fb25c093bb2907c3576d45accc8646a8ad
Zafar <zafar.a.ali2@gmail.com>
2019-04-18 13:38:44 -0400

    Created Icon, APK, and Changed Colors

--- app/src/main/java/com/zafar/counter/MainActivity.kt
+++ app/src/main/java/com/zafar/counter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zafar.counter
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,29 +10,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var count = 0
+    val PREFS_FILENAME = "com.zafar.counter.prefs"
+    val COUNT_PREF = "counter_val"
+
+    var prefs: SharedPreferences? = null
 
     fun reset(view : View){
         count = 0
         textView.setText(""+count)
 
+        // Put count in SharedPrefs
+        val editor = prefs!!.edit()
+        editor.putInt(COUNT_PREF, count)
+        editor.apply()
     }
 
     fun plusOne(view : View){
         count++
         textView.setText(""+count)
 
+        // Put count in SharedPrefs
+        val editor = prefs!!.edit()
+        editor.putInt(COUNT_PREF, count)
+        editor.apply()
     }
 
     fun minusOne(view : View){
         count--
         textView.setText(""+count)
 
+        // Put count in SharedPrefs
+        val editor = prefs!!.edit()
+        editor.putInt(COUNT_PREF, count)
+        editor.apply()
     }
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get count from SharedPrefs
+        prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
+        count = prefs!!.getInt(COUNT_PREF, count)
 
         var textView = findViewById<TextView>(R.id.textView)
         textView.setText(""+count)
