375527a79f89aa2ae61881cd2797eadfc82d7a6b
Zafar <zafar.a.ali2@gmail.com>
2019-04-18 13:02:56 -0400

    Button Layout and Functions
    - reset, plusOne, minusOne

--- app/src/main/java/com/zafar/counter/MainActivity.kt
+++ app/src/main/java/com/zafar/counter/MainActivity.kt
@@ -2,11 +2,39 @@ package com.zafar.counter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
+    fun reset(view : View){
+        count = 0
+        textView.setText(""+count)
+
+    }
+
+    fun plusOne(view : View){
+        count++
+        textView.setText(""+count)
+
+    }
+
+    fun minusOne(view : View){
+        count--
+        textView.setText(""+count)
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textView = findViewById<TextView>(R.id.textView)
+        textView.setText(""+count)
+
     }
 }
